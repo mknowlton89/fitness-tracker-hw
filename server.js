7269e1d2db3661a6cc04df134d490efbb7e98f45
Michael Knowlton <mknowlton89@gmail.com>
2021-05-06 21:08:21 -0400

    Updated the mongoose.connect code

--- server.js
+++ server.js
@@ -13,10 +13,15 @@ app.use(express.json());
 // app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, 'public')))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.use(require("./routes/api.js"));
 
